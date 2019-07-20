56c1452bff1fb3d5ec0927dc6dda3bea7646b322
Mod Nattasit <mod.nattasit@gmail.com>
2019-07-20 15:46:02 +0700

    Update Project

---
+++ src/HuahuiSite.Core/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuahuiSite.Core.Entities
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int SaleId { get; set; }
+    }
+}
---
+++ src/HuahuiSite.Core/Entities/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuahuiSite.Core.Entities
+{
+    public partial class Sale
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
