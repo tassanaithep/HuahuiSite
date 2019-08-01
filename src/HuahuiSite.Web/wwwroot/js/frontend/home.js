06c49c6a00edee7a2c1bebd9ec8c2d68b671ae5f
Mod Nattasit <mod.nattasit@gmail.com>
2019-08-02 01:11:24 +0700

    Update Project

---
+++ src/HuahuiSite.Web/wwwroot/js/frontend/home.js
@@ -0,0 +1,122 @@
+﻿// #region On Load
+
+/**
+  * @desc On Page Load
+  * @author Mod Nattasit mod.nattasit@gmail.com
+*/
+$(function () {
+
+});
+
+// #endregion
+
+// #region Functions
+
+/**
+  * @desc Open Modal of Product
+  * @param {Object} e - Element of Open Modal Button
+  * @author Mod Nattasit mod.nattasit@gmail.com
+*/
+OpenProductModal = (e) => {
+    let $form = $(e).closest(".form-product-item");
+
+    let $formId = $form.find("[name='hid-form-product-id']").val();
+    let $productId = $form.find("[name='ProductId']").val();
+    let $name = $form.find("[name='ProductName']").val();
+    let $unitPrice = $form.find("[name='ProductUnitPrice']").val();
+    let $pictureFileName = $form.find("[name='ProductPictureFileName']").val();
+
+    let qty = null;
+
+    for (var i = 0; i < _cartItemList.length; i++) {
+        if (_cartItemList[i].productId === parseInt($productId)) {
+            qty = _cartItemList[i].quantity;
+            break;
+        }
+    }
+
+    $("body").append(`
+        <div class="modal fade" id="modal-product" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="ClearProductModal()">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <div class="modal_body">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-5 col-md-5 col-sm-12">
+                                    <div class="modal_tab">
+                                        <div class="tab-content product-details-large">
+                                            <div class="tab-pane fade show active" id="tab1" role="tabpanel">
+                                                <div class="modal_tab_img">
+                                                    <a href="#"><img src="/images/upload/${ $pictureFileName }" alt=""></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-7 col-md-7 col-sm-12">
+                                    <div class="modal_right">
+                                        <div class="modal_title mb-10">
+                                            <h2>${ $name }</h2>
+                                        </div>
+                                        <div class="modal_price mb-10">
+                                            <span class="new_price">$${ $unitPrice }</span>
+                                            <span class="old_price">$78.99</span>
+                                        </div>
+                                        <div class="modal_description mb-15">
+                                        </div>
+                                        <div class="variants_selects">
+                                            <div class="modal_add_to_cart">
+                                                <form action="#">
+                                                    <input min="0" max="100" step="2" value="${ (qty !== null ? qty : 1) }" type="number">
+                                                    <button type="button" onclick="ProductModalSubmit('${ $formId }')">add to cart</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+`);
+
+    $("#modal-product").modal("show");
+};
+
+ClearProductModal = () => {
+    $("#modal-product").remove();
+    $(".modal-backdrop").remove();
+    $("body").removeClass("modal-open");
+    $("body").removeProp("style");
+};
+
+ProductModalSubmit = (formId) => {
+    $("#" + formId).find(".btn-submit").click();
+};
+
+// #endregion
+
+// #region Render
+  
+// #endregion
+
+// #region Affect
+
+// #endregion
+
+// #region Action
+
+// #endregion
+
+// #region Validate
+
+// #endregion
+
+// #region Response
+
+// #endregion
