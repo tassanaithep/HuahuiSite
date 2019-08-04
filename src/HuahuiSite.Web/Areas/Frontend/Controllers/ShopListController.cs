234d67f01e8b5bf0316a331b84092be16da64608
Tassanai Thephinlap <tassanat@scg.com>
2019-08-04 19:08:21 +0700

    update shop

--- src/HuahuiSite.Web/Areas/Frontend/Controllers/ShopListController.cs
+++ src/HuahuiSite.Web/Areas/Frontend/Controllers/ShopListController.cs
@@ -23,25 +23,43 @@ namespace HuahuiSite.Web.Areas.Frontend.Controllers
             _shopListService = shopListService;
         }
         #endregion
-        public IActionResult Index(string param)
+        public IActionResult Index(string param,string param2)
         {
             MainViewModel mainViewModel = new MainViewModel();
          //   ShopListViewModel shopListViewModel = new ShopListViewModel();
 
-
             try
             {
                _shopListService.GetShopList(ref mainViewModel);
-                if(param != null)
+
+                //set default param = first
+                if(param== null)
                 {
+                    param = mainViewModel.ProductCategorieList.FirstOrDefault().Code;
+                }
+               
+                // หา productgroup name 
+
+                if (param != null && param2==null)
+                {
+                    
+
                     mainViewModel.ShopListViewModel.ProductList = mainViewModel.ShopListViewModel.ProductList.Where(x => x.ProductCategorieCode == param).ToList();
+                    mainViewModel.ProductGroupList = mainViewModel.ProductGroupList.Where(x => x.ProductCategorieCode == param).ToList();
+
+                    mainViewModel.ShopListViewModel.Param = param;
+                }
+                else if(param != null && param2 != null)
+                {
+                    //param2 = mainViewModel.ProductGroupList.Where(m=>m.ProductCategorieCode==param2).FirstOrDefault().Code;
+                    //  mainViewModel.ProductGroupList =
+                    mainViewModel.ShopListViewModel.ProductList = mainViewModel.ShopListViewModel.ProductList.Where(x => x.ProductCategorieCode == param && x.ProductGroupCode==param2).ToList();
+                    mainViewModel.ProductGroupList = mainViewModel.ProductGroupList.Where(x => x.ProductCategorieCode == param).ToList();
+                    //  mainViewModel.ProductGroupList.ToList().Clear();
+
+                    // _shopListService.GetShopList(ref shopListViewModel);
 
                 }
-                //else
-                //{
-                //    _shopListService.GetShopList(ref shopListViewModel);
-
-                //}
 
 
             }
